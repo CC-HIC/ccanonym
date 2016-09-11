fe06f05d1488e93373c54f1d66c24ff4f144358d
Steve Harris <steve@Steves-MacBook-Pro.local>
2016-09-12 00:28:07 +0100

    utility to convert YAML fields to table

---
+++ tmp/yaml2table.R
@@ -0,0 +1,19 @@
+library(yaml)
+library(purrr)
+library(data.table)
+anon.dict <- yaml::yaml.load_file("data/ANON_REF.yaml")
+anon.dict <- anon.dict %>% map(function(x) list(
+          Anonymisation = x$Anonymisation,
+          dataItem = x$dataItem,
+          NHICcode = x$NHICcode
+          ))
+ll <- length(anon.dict)
+dt <- data.table(matrix(unlist(anon.dict), nrow=ll, byrow=T),stringsAsFactors=FALSE)
+names(dt) <- c("Anonymisation", "dataItem", "NHICcode")
+dt[, Anonymisation := factor(Anonymisation,
+    levels = c("Direct identifier", "Key variable", "Non-identifying"),
+    ordered=TRUE)]
+setcolorder(dt, c("Anonymisation", "NHICcode", "dataItem"))
+dt <- dt[order(Anonymisation, NHICcode)]
+dt
+fwrite(dt, file="data/ANON_REF.csv")
