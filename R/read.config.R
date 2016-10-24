d1bf989916467a6d773680f3117eca53309fede3
Sinan Shi <s.shi@ucl.ac.uk>
2016-10-24 10:05:44 +0100

    remove NAs in numeric vectors

--- R/read.config.R
+++ R/read.config.R
@@ -10,31 +10,24 @@ create.sdc <- function(ccd, path, remove.alive=T, verbose=F) {
         demg <- demg[DIS=="D"]
 
     conf <- yaml.load_file(path)
+
     keyv <- conf$keyVars
     numv <- names(conf$numVars)
     datetimev <- names(conf$datetimeVars)
+    sensv <- conf$sensibleVar
 
     numv <- c(numv, datetimev)
     demg <- data.frame(convert.numeric.datetime(demg, datetimev))
-    
-    # check the data type and avoid processing on the all NA columns.
-    new.numv <- vector()
-    for (i in seq(numv)) {
-        if (!is.numeric(demg[[numv[i]]])) {
-            stop(paste(numv[i], "has to be in numeric type."))
-        }
-        if (!all(is.na(demg[[numv[i]]])) & 
-            length(unique(demg[[numv[[i]]]]) != 1))
-            new.numv <- c(new.numv, numv[i])
-    }
-    numv <- new.numv
+    numv <- remove.all.null.numvar(demg, numv)
+#    demg <- fill.na.numvar(demg, numv)
+    print(demg[, numv])
 
-    sdc <<- createSdcObj(demg, keyVars=keyv, numVars=numv,
+    sdc <- createSdcObj(demg, keyVars=keyv, numVars=numv,
                         sensibleVar=conf$sensibleVar)
 
     sdc <- localSuppression(sdc)
-    sdc <- addNoise(sdc, noise=1)
-    
+    sdc <- sdc.numvar(sdc, conf, numv)
+
     if (verbose)
         print(sdc)
 
@@ -44,6 +37,65 @@ create.sdc <- function(ccd, path, remove.alive=T, verbose=F) {
 
     return(demg)
 }
+
+sdc.numvar <- function(sdc, conf, numv) {
+    numconf <- append(conf$numVars, conf$datetimeVars)
+    for (item in numv) {
+        print(item)
+        operations  <- names(numconf[[item]])
+        for (op in operations) {
+            FUN <- eval(parse(text=op))
+            args <- 
+                append(numconf[[item]][[op]], list(obj=sdc, variables=item))
+            sdc <- do.call(FUN, args)
+        }
+    }
+    sdc
+}
+
+#' @description
+#' sdcMicro package failed to create the sdc object with numerical vector where NAs are 
+#' presented. To overcome this problem, we fill the NAs
+#' with the mean value of the vector and restore NAs by the end of the whole
+#' process. 
+#' @param numdata is a data.frame 
+#' @return a data.frame where the NAs in the columns are filled with the mean
+#' value. 
+fill.na.numvar <- function(data, numv) {
+    data[, numv]<- 
+        apply(data[, numv], 2, 
+              function(x) {
+                  x[is.na(x)] <- mean(x, na.rm=T)
+                  return(x)
+              })
+    data
+}
+
+
+
+na.index <- function(data){
+    apply(data, 2,
+          function(x) {
+              x[!is.na(x)] <- 1
+              x
+          })
+}
+remove.all.null.numvar <- function(data, numv) {
+    # check the data type and avoid processing on the 
+    # all NA columns.
+    new.numv <- vector()
+    for (i in seq(numv)) {
+        if (!is.numeric(data[[numv[i]]])) {
+            stop(paste(numv[i], "has to be in numeric type."))
+        }
+        if (!all(is.na(data[[numv[i]]])) & 
+            length(unique(data[[numv[[i]]]]) != 1))
+            new.numv <- c(new.numv, numv[i])
+    }
+    new.numv
+}
+
+
 
 
 
--- tests/testthat/test-create.sdc.R
+++ tests/testthat/test-create.sdc.R
@@ -2,7 +2,8 @@ context("create sdc object from the yaml configuration file")
 
 test_that("create sdc object", {
     ccd <- xml2Data("../data/test_data_anonym.xml")
-    demg <- data.table(suppressWarnings(sql.demographic.table(ccd)))
+    demg <- suppressWarnings(sql.demographic.table(ccd))
+    demg <<- data.frame(demg)
     conf <- yaml.load_file('../../data/test.yaml')
 
 
@@ -14,10 +15,16 @@ test_that("create sdc object", {
     var <- c(keydt, keyvar, numvar)
 
     print("")
-    print(var)
-    print(demg[, var, with=F])
+#    print(var)
+#    print(demg[, var, with=F])
 
-    sdc <- create.sdc(ccd, '../../data/test.yaml', verbose=T)
+    demg2 <<- create.sdc(ccd, '../../data/test.yaml', verbose=T)
 #    print(sdc)
 #    print(sdc[, var])
+})
+
+
+test_that("numeric variables sdc control", {
+#    conf <- yaml.load_file('../../data/test.yaml')
+#    num.vars.sdc(demg, conf$datetimeVars)
 })
